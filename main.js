d42fe4b7b52c72529ce64a222bf6570cc674b118
LAPTOP-4DFHSMCJ\lenovo <819239765@qq.com>
2016-09-22 21:29:15 +0800

    Finish Homewok 2

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello,Menghan");
+function print(n) {
+    for (var a = 1; a <= n; a++) {
+        var no = "";
+        for (var b = 1; b <= n - a; b++) {
+            no = no + " ";
+        }
+        for (var c = 1; c <= 2 * a - 1; c++) {
+            var gold = "*";
+            no = no + gold;
+        }
+        console.log(no);
+    }
+}
+var num = process.argv[2];
+print(num);
--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-console.log("Hello,Menghan");
+function print(n : number) : void{
+for (var a = 1; a <= n; a++) {
+    var no : string = "";
+
+    for (var b = 1; b <= n - a; b++) {
+         no = no + " ";     
+    }
+    
+    for (var c = 1; c <= 2 * a - 1; c++) {
+         var gold : string = "*";
+         no = no + gold;
+    }
+    console.log(no); 
+  }
+
+}
+
+ var num:number = process.argv[2];
+ print(num);
